391370350b10992e16b11527aa71d8ec7ceb0611
jangcodes <jangjong87@hotmail.com>
2023-01-29 07:42:55 -0500

    Made changes so i dont have to enter inital amounts multiple times

--- ConsoleMortgageCalculator/Program.cs
+++ ConsoleMortgageCalculator/Program.cs
@@ -6,25 +6,22 @@ namespace ConsoleMortgageCalculator
     {
         static void Main(string[] args)
         {
-            while(true)
+            Console.WriteLine("Mortgage Calculator!");
+
+            Console.WriteLine("Enter remaining amount:");
+            var initialMortgageAmount = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter APR:");
+            var apr = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Last Paid Interest Amount:");
+            var lastPaidInterest = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Last Paid Principal Amount:");
+            var lastPaidPrincipal = Convert.ToDecimal(Console.ReadLine());
+
+            while (true)
             {
-                Console.WriteLine("Mortgage Calculator!");
-
-
-                Console.WriteLine("Enter remaining amount:");
-                var mortgageAmount = Convert.ToDecimal(Console.ReadLine());
-
-
-                Console.WriteLine("Enter APR:");
-                var apr = Convert.ToDecimal(Console.ReadLine());
-
-
-                Console.WriteLine("Last Paid Interest Amount:");
-                var lastPaidInterest = Convert.ToDecimal(Console.ReadLine());
-
-                Console.WriteLine("Last Paid Principal Amount:");
-                var lastPaidPrincipal = Convert.ToDecimal(Console.ReadLine());
-
                 Console.WriteLine("Extra Paid Principal Amount:");
                 var extraPayment = Convert.ToDecimal(Console.ReadLine());
 
@@ -33,6 +30,7 @@ namespace ConsoleMortgageCalculator
                 int monthsPaid = 0;
                 decimal totalInterest = 0;
 
+                var mortgageAmount = initialMortgageAmount;
                 while (mortgageAmount > 0)
                 {
                     var monthlyInterest = Math.Round(mortgageAmount * (apr / 100 / 12), 2);
@@ -41,7 +39,6 @@ namespace ConsoleMortgageCalculator
 
                     var principal = currentPayment - monthlyInterest;
 
-
                     mortgageAmount -= (principal + extraPayment);
 
                     monthsPaid++;
@@ -49,11 +46,10 @@ namespace ConsoleMortgageCalculator
                     Console.WriteLine($"Month {monthsPaid} - Principal: ${(principal + extraPayment)}, Interest: ${monthlyInterest}, Remaining: ${mortgageAmount} ");
                 }
 
-
                 Console.WriteLine(Math.Floor(monthsPaid / 12f) + " Years and " + monthsPaid % 12 + " Months");
                 Console.WriteLine("Total Interest Paid: " + totalInterest);
-
-                Console.WriteLine("Press any key to exit");
+                Console.WriteLine("");
+                Console.WriteLine("Press any key to use different additional principal amount");
                 Console.ReadKey();
                 Console.WriteLine("");
                 Console.WriteLine("");                
